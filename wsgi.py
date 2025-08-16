bc2b0a2340c0727d4382d573b5f67258edbf31e7
Ankit Anand 193615 <ankit.anand@lenskart.com>
2025-08-16 16:13:25 +0530

    updating settings.py

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for nso_vault project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nso_vault.settings')
+
+application = get_wsgi_application()
+
+# This is the line you need to add for Vercel
+app = application
