96a377f471733ae722c7ff158d4f1d58c27cbe1f
Ankit Anand 193615 <ankit.anand@lenskart.com>
2025-08-17 13:52:09 +0530

    code refactor

--- build.sh
+++
@@ -1,5 +0,0 @@
-                                                        #!/usr/bin/env bash
-set -o errexit
-
-/usr/local/bin/python3 -m pip install -r requirements.txt
-/usr/local/bin/python3 manage.py collectstatic --noinput
